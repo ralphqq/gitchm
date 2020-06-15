a05a9b40ea206696952a504b2db0bcfd37ef9539
Ralph Quirequire <ralph.quirequire2012@gmail.com>
2020-06-15 15:11:29 +0800

    Define constructor for main class

---
+++ app/mirror.py
@@ -0,0 +1,16 @@
+import logging
+import os
+
+from git import Repo
+
+
+logger = logging.getLogger()
+
+
+class CommitHistoryMirror:
+
+    def __init__(self, source_workdir: str, author_emails: list) -> None:
+        self.source_workdir = source_workdir
+        self.author_emails = author_emails
+        self.parent_dir, self.source_repo_name = os.path.split(source_workdir)
+        self.source_repo = Repo(source_workdir)
