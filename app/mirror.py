80b5be3bd6e268e072b846c5e5d2ad7cc2576e03
Ralph Quirequire <ralph.quirequire2012@gmail.com>
2020-06-16 07:11:17 +0800

    Remove author_emails as constructor parameter

--- app/mirror.py
+++ app/mirror.py
@@ -9,8 +9,7 @@ logger = logging.getLogger()
 
 class CommitHistoryMirror:
 
-    def __init__(self, source_workdir: str, author_emails: list = None) -> None:
+    def __init__(self, source_workdir: str) -> None:
         self.source_workdir = source_workdir
-        self.author_emails = author_emails
         self.parent_dir, self.source_repo_name = os.path.split(source_workdir)
         self.source_repo = Repo(source_workdir)
