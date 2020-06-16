d7d805fdf12b938d5a0bbc8a6466ca56014c1747
Ralph Quirequire <ralph.quirequire2012@gmail.com>
2020-06-16 12:05:56 +0800

    Modify mirror tests to include separation of repo initialization logic

--- app/mirror.py
+++ app/mirror.py
@@ -11,5 +11,10 @@ class CommitHistoryMirror:
 
     def __init__(self, source_workdir: str) -> None:
         self.source_workdir = source_workdir
-        self.parent_dir, self.source_repo_name = os.path.split(source_workdir)
-        self.source_repo = Repo(source_workdir)
+        self._init_source_repo()
+
+    def _init_source_repo(self) -> None:
+        self.parent_dir, self.source_repo_name = os.path.split(
+            self.source_workdir
+        )
+        self.source_repo = Repo(self.source_workdir)
--- tests/test_mirror.py
+++ tests/test_mirror.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 
 from git import Repo
 import pytest
@@ -7,6 +8,33 @@ from app.mirror import CommitHistoryMirror
 
 
 class TestSourceRepoAccess:
+
+    @pytest.fixture
+    def mock_init_repos(self, mocker):
+        """Mocks the repo _init methods called in constructor."""
+        mocker.patch.object(CommitHistoryMirror, '_init_source_repo')
+
+    @pytest.fixture
+    def modified_chm(self, init_source_repo):
+        """Modifies CommitHistoryMirror class."""
+        source_workdir, parent_dir_path, commit_data = init_source_repo
+
+        class ModifiedCHM(CommitHistoryMirror):
+            """Overrides parent class's __init__ method.
+
+            This makes it possible to separately test each 
+            method called in the original class's __init__ method.
+            """
+
+            def __init__(self):
+                self.source_workdir = source_workdir
+
+        return {
+            'mirror': ModifiedCHM(),
+            'source_workdir': source_workdir,
+            'parent_dir': parent_dir_path,
+            'commits': commit_data,
+        }
 
     @pytest.fixture(scope='class')
     def chm(self, init_source_repo):
@@ -20,12 +48,20 @@ class TestSourceRepoAccess:
             'commits': commit_data,
         }
 
-    def test_source_repo_access(self, chm):
-        mirror = chm['mirror']
-        all_commits = list(mirror.source_repo.iter_commits('master'))
-        source_repo_name = os.path.split(chm['source_workdir'])[-1]
+    def test_mirror_initialization(self, mock_init_repos):
+        some_dir = '/tmp/foo/bar'
+        mirror = CommitHistoryMirror(some_dir)
 
-        assert len(all_commits) == len(chm['commits'])
-        assert mirror.source_workdir == chm['source_workdir']
+        assert mirror.source_workdir == some_dir
+        assert mirror._init_source_repo.called
+
+    def test_source_repo_init(self, modified_chm):
+        mirror = modified_chm['mirror']
+        mirror._init_source_repo()
+        all_commits = list(mirror.source_repo.iter_commits('master'))
+        source_repo_name = os.path.split(modified_chm['source_workdir'])[-1]
+
+        assert len(all_commits) == len(modified_chm['commits'])
+        assert mirror.source_workdir == modified_chm['source_workdir']
         assert mirror.source_repo_name == source_repo_name
-        assert mirror.parent_dir == chm['parent_dir']
+        assert mirror.parent_dir == modified_chm['parent_dir']
