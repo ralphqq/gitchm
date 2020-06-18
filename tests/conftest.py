7f19af1cdca224645c137c48cb3e65d3659fb5f9
Ralph Quirequire <ralph.quirequire2012@gmail.com>
2020-06-18 10:59:46 +0800

    Rewrite commit loading logic as separate helper function

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,13 @@
-from datetime import datetime
-import json
 import os
 import tempfile
 
-from git import Actor, Repo
+from git import Repo
 import pytest
 
 from app.utils import create_dir, delete_dir
+from tests.utils import make_commits
 
 
-TEST_DIR = os.path.abspath(os.path.dirname(__file__))
-COMMIT_DATAFILE = os.path.join(TEST_DIR, 'data/commits.json')
 PARENT_DIR = 'chm-test-session'
 SOURCE_WORKDIR = 'project-src'
 
@@ -50,48 +47,11 @@ def init_source_repo(init_chm_test_session):
         on_conflict='replace'
     )
 
-    # Load data for creating dummy commits
-    commits = []
-    with open(COMMIT_DATAFILE, 'r', encoding='utf-8') as f:
-        commits = json.load(f)
-
-    # Sort in chronological order
-    commits = sorted(commits, key=lambda x: x['timestamp'])
-
     # Create and initiallize source repo
     repo = Repo.init(source_workdir_path)
 
-    # Simulate git add-commit workflow for each commit item
-    for i, commit_item in enumerate(commits):
-        message = commit_item['message']
-        commit_dt = datetime.fromtimestamp(commit_item['timestamp']).isoformat()
-
-        # Create new file
-        fname = f'{i:05d}.txt'
-        fpath = os.path.join(source_workdir_path, fname)
-        with open(fpath, 'w', encoding='utf-8') as f:
-            # Write commit message as file content
-            f.write(message)
-
-        # Create author and committer
-        author = Actor(
-            name=commit_item['author_name'],
-            email=commit_item['author_email']
-        )
-        committer = Actor(
-            name=commit_item['committer_name'],
-            email=commit_item['committer_email']
-        )
-
-        # Stage and commit the created file
-        repo.index.add([fpath])
-        repo.index.commit(
-            message=message,
-            author=author,
-            author_date=commit_dt,
-            committer=committer,
-            commit_date=commit_dt
-        )
+    # Make the commits
+    commits = make_commits(repo)
 
     yield (
         source_workdir_path,
--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,84 @@
+"""
+Some helper functions and classes used throughout the test suite
+
+Helper functions:
+    make_commits(exclude_n_commits: int = 0) -> list:
+
+Helper Classes:
+    ModifiedCHM
+"""
+from datetime import datetime
+import json
+import os
+
+from git import Actor, Repo
+
 from app.mirror import CommitHistoryMirror
 
+
+TEST_DIR = os.path.abspath(os.path.dirname(__file__))
+COMMIT_DATAFILE = os.path.join(TEST_DIR, 'data/commits.json')
+
+
+# Helper functions
+
+def make_commits(repo: Repo, exclude_commits: int = 0) -> list:
+    """Loads commit data from JSON file and makes commits in given repo.
+
+    Args:
+        repo (`Repo`): git repo instance where commits will be made
+        exclude_commits (int): number of latest commits to exclude
+
+    Returns:
+        list: list of dicts representing commits made
+    """
+    commits_fetched = []
+
+    # Load data for creating dummy commits
+    with open(COMMIT_DATAFILE, 'r', encoding='utf-8') as f:
+        commits_fetched = json.load(f)
+
+    # Sort in chronological order
+    # and slice list based on excluded commits
+    commits_fetched = sorted(commits_fetched, key=lambda x: x['timestamp'])
+    commits_made = commits_fetched[:len(commits_fetched) - exclude_commits]
+
+    # Simulate git add-commit workflow for each commit item
+    for i, commit_item in enumerate(commits_made):
+        message = commit_item['message']
+        commit_dt = datetime.fromtimestamp(commit_item['timestamp']).isoformat()
+
+        # Create new file
+        fname = f'{i:05d}.txt'
+        fpath = os.path.join(repo.working_dir, fname)
+        with open(fpath, 'w', encoding='utf-8') as f:
+            # Write commit message as file content
+            f.write(message)
+
+        # Create author and committer
+        author = Actor(
+            name=commit_item['author_name'],
+            email=commit_item['author_email']
+        )
+        committer = Actor(
+            name=commit_item['committer_name'],
+            email=commit_item['committer_email']
+        )
+
+        # Stage and commit the created file
+        repo.index.add([fpath])
+        repo.index.commit(
+            message=message,
+            author=author,
+            author_date=commit_dt,
+            committer=committer,
+            commit_date=commit_dt
+        )
+
+    return commits_made
+
+
+# Helper classes
 
 class ModifiedCHM(CommitHistoryMirror):
     """Overrides parent class's __init__ method.
