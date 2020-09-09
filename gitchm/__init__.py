1b403efb30fda127333575c131723a75200a0ab3
Ralph Quirequire <ralph.quirequire2012@gmail.com>
2020-09-09 22:37:55 +0800

    Skip autoformatting for package details items in __init__.py file

--- gitchm/__init__.py
+++ gitchm/__init__.py
@@ -1,10 +1,10 @@
-__title__ = "gitchm"
-__url__ = "https://github.com/ralphqq/gitchm"
-__license__ = "MIT License"
-__package__ = "gitchm"
-__version__ = "0.1.0"
-__short_description__ = (
-    "CLI tool that copies commit history without reproducing the working tree"
-)
-__author__ = "Ralph Quirequire"
-__author_email__ = "ralph.quirequire2012@gmail.com"
+# fmt: off
+__title__ = 'gitchm'
+__url__ = 'https://github.com/ralphqq/gitchm'
+__license__ = 'MIT License'
+__package__ = 'gitchm'
+__version__ = '0.1.0'
+__short_description__ = 'CLI tool that copies commit history without reproducing the working tree'
+__author__ = 'Ralph Quirequire'
+__author_email__ = 'ralph.quirequire2012@gmail.com'
+# fmt: on
